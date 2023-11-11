25db129009a28ca92b0b4baef759d20bb6795250
“DangTranQL” <tranquangluandang762@gmail.com>
2023-11-11 09:22:46 -0500

    search bar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import MovieList from '../components/MovieList';
 import { styled } from '@mui/system';
+import PrimarySearchAppBar from '../components/Search';
 
 const PageHome = styled('div')({
     display: 'flex',
@@ -12,6 +13,7 @@ const PageHome = styled('div')({
 export default function HomePage() {
   return (
     <PageHome>
+      <PrimarySearchAppBar/>
       <MovieList url="https://api.themoviedb.org/3/movie/popular"/>
       <MovieList url="https://api.themoviedb.org/3/movie/now_playing"/>
       <MovieList url="https://api.themoviedb.org/3/movie/top_rated"/>
